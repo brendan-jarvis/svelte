8006acb17cd59dd1cf451d8d13eaae3ed954ea2c
Brendan Jarvis <jarvisbrendanj@gmail.com>
2023-02-07 14:05:04 +1300

    Import component

---
+++ src/components/nested.svelte
@@ -0,0 +1,2 @@
+<h2>Nested component</h2>
+<p>This was loaded from a nested component.</p>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,16 +1,26 @@
 <script>
+	import Nested from '../components/nested.svelte';
+
 	const name = 'world';
-  const placekitten = { src: 'https://placekitten.com/500/500', alt: 'A cute kitten' };
+	const placekitten = { src: 'https://placekitten.com/500/500', alt: 'A cute kitten' };
 </script>
+
+<h1>Hello {name.toUpperCase()}!</h1>
+
+<p>This is some red text.</p>
+
+<img src={placekitten.src} alt={placekitten.alt} />
+
+<Nested />
 
 <style>
 	img {
 		border-radius: 50%;
-    border: 1px solid #0ff;
-    box-shadow: 0 0 5px #f0f
+		border: 1px solid #0ff;
+		box-shadow: 0 0 5px #f0f;
+	}
+
+	p {
+		color: red;
 	}
 </style>
-
-<h1>Hello {name.toUpperCase()}!</h1>
-
-<img src={placekitten.src} alt={placekitten.alt}>

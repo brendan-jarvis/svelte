6b693f2329c540e77e7b6d528b619bf4d7971036
Brendan Jarvis <jarvisbrendanj@gmail.com>
2023-02-07 17:05:32 +1300

    Bindings

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,7 @@
 	import Clicker from '../components/clicker.svelte';
 	import Nav from '../components/nav.svelte';
 
-	const name = 'world';
+	let name = 'world';
 	const placekitten = { src: 'https://placekitten.com/500/500', alt: 'A cute kitten' };
 	const html = '<p>Some HTML. We can use this feature to expose our website to XSS attacks.</p>';
 </script>
@@ -10,6 +10,9 @@
 <Nav />
 
 <h1>Hello {name.toUpperCase()}!</h1>
+
+<label for="name">Change name: </label>
+<input type="text" bind:value={name} />
 
 {@html html}
 

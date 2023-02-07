ea3e1f34450b72fc55f0d935df5899235a58d000
Brendan Jarvis <jarvisbrendanj@gmail.com>
2023-02-07 13:44:28 +1300

    Adding data

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,8 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<script>
+	const name = 'brendan';
+</script>
+
+<h1>Hello {name.toUpperCase()}!</h1>

9f8f83c1cc67611f65420009235168c717f2e90d
Brendan Jarvis <jarvisbrendanj@gmail.com>
2023-02-14 21:50:24 +1300

    JS typewriter transition

--- src/components/clicker.svelte
+++ src/components/clicker.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+	import { typewriter } from '../utils';
+
 	let count = 0;
 	let clicked = false;
 	$: doubled = count * 2;
@@ -40,4 +42,14 @@
 	<button on:click={() => (clicked = true)}>Click me!</button>
 {:else}
 	<button on:click={() => (clicked = false)}>Unclick me!</button>
+{/if}
+
+{#if clicked}
+	<p transition:typewriter={{ speed: 1 }}>
+		ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+		dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+		aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+		cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+		culpa qui officia deserunt mollit anim id est laborum.
+	</p>
 {/if}
---
+++ src/utils.ts
@@ -0,0 +1,21 @@
+export const typewriter = (node: HTMLParagraphElement, {speed = 1}: { speed: number; }) => {
+  const valid = (
+    node.childNodes.length === 1 &&
+    node.childNodes[0].nodeType === Node.TEXT_NODE
+  );
+
+  if (!valid || node.textContent === null) {
+    throw new Error(`This transition only works on elements with a single text node child`);
+  }
+
+  const text = node.textContent;
+  const duration = text.length / (speed * 0.2);
+
+  return {
+    duration,
+    tick: (t: number) => {
+      const i = Math.trunc(text.length * t);
+      node.textContent = text.slice(0, i);
+    }
+  };
+};

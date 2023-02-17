7c3868967e3d8db50823faf30aa558a98050ff9b
Brendan Jarvis <jarvisbrendanj@gmail.com>
2023-02-17 13:42:05 +1300

    Redo layout styling

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -17,3 +17,43 @@
 </main>
 
 <footer>Made by Brendan Jarvis while learning Svelte in 2023</footer>
+
+<style>
+	nav {
+		display: flex;
+		justify-content: space-around;
+		align-items: center;
+		width: 100%;
+		height: 50px;
+		background-color: var(--polar-night-3);
+		color: var(--snow-storm-1);
+		font-family: 'Roboto Mono', monospace;
+		font-size: 1.3rem;
+		padding: 0.3rem;
+	}
+
+	nav a {
+		color: var(--snow-storm-3);
+		font-weight: bold;
+		text-decoration: none;
+	}
+
+	nav a:hover {
+		color: var(--frost-2);
+	}
+
+	nav,
+	footer {
+		text-transform: uppercase;
+		width: 100%;
+	}
+
+	footer {
+		font-size: smaller;
+		text-align: center;
+		padding: 2rem;
+		color: var(--frost-1);
+		background-color: var(--polar-night-3);
+		font-family: 'Roboto Mono', monospace;
+	}
+</style>

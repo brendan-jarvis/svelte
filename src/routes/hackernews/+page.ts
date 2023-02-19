f8a9a5ee0c3d6ee819da91491bff883a4f5e30f7
Brendan Jarvis <jarvisbrendanj@gmail.com>
2023-02-20 00:11:11 +1300

    Run everything at once with Promise.all()

--- src/routes/hackernews/+page.ts
+++ src/routes/hackernews/+page.ts
@@ -2,17 +2,17 @@ import type { PageLoad } from './$types';
 import { error } from '@sveltejs/kit';
 
 export const load = (async ({ fetch }) => {
-	const fetchTopPosts = async () => {
-		const res = await fetch(`https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty`);
-		const topStories = await res.json();
+	const fetchStories = async (url: string) => {
+		const res = await fetch(url);
+		const storyIds = await res.json();
 
-		const topFiftyStories = topStories.slice(0, 50);
+		const fiftyStories = storyIds.slice(0, 50);
 
 		if (!res.ok) {
-			throw error(res.status, 'Something went wrong fetching top stories from Hacker News');
+			throw error(res.status, `Something went wrong fetching stories from Hacker News: ${url}`);
 		}
 
-		const storyDataPromises = topFiftyStories.map(async (story: string) => {
+		const storyDataPromises = fiftyStories.map(async (story: string) => {
 			const storyRes = await fetch(`https://hacker-news.firebaseio.com/v0/item/${story}.json`);
 			if (!storyRes.ok) {
 				throw error(storyRes.status, 'Something went wrong fetching a story from Hacker News');
@@ -25,55 +25,15 @@ export const load = (async ({ fetch }) => {
 		return storyData;
 	};
 
-	const fetchBestPosts = async () => {
-		const res = await fetch(`https://hacker-news.firebaseio.com/v0/beststories.json?print=pretty`);
-		const bestStories = await res.json();
-
-		const fiftyBestStories = bestStories.slice(0, 50);
-
-		if (!res.ok) {
-			throw error(res.status, 'Something went wrong fetching best stories from Hacker News');
-		}
-
-		const storyDataPromises = fiftyBestStories.map(async (story: string) => {
-			const storyRes = await fetch(`https://hacker-news.firebaseio.com/v0/item/${story}.json`);
-			if (!storyRes.ok) {
-				throw error(storyRes.status, 'Something went wrong fetching a story from Hacker News');
-			}
-			return storyRes.json();
-		});
-
-		const storyData = await Promise.all(storyDataPromises);
-
-		return storyData;
-	};
-
-	const fetchNewPosts = async () => {
-		const res = await fetch(`https://hacker-news.firebaseio.com/v0/newstories.json?print=pretty`);
-		const newStories = await res.json();
-
-		const fiftyNewStories = newStories.slice(0, 50);
-
-		if (!res.ok) {
-			throw error(res.status, 'Something went wrong fetching new stories from Hacker News');
-		}
-
-		const storyDataPromises = fiftyNewStories.map(async (story: string) => {
-			const storyRes = await fetch(`https://hacker-news.firebaseio.com/v0/item/${story}.json`);
-			if (!storyRes.ok) {
-				throw error(storyRes.status, 'Something went wrong fetching a story from Hacker News');
-			}
-			return storyRes.json();
-		});
-
-		const storyData = await Promise.all(storyDataPromises);
-
-		return storyData;
-	};
+	const [topStories, bestStories, newStories] = await Promise.all([
+		fetchStories('https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty'),
+		fetchStories('https://hacker-news.firebaseio.com/v0/beststories.json?print=pretty'),
+		fetchStories('https://hacker-news.firebaseio.com/v0/newstories.json?print=pretty')
+	]);
 
 	return {
-		topStories: fetchTopPosts(),
-		bestStories: fetchBestPosts(),
-		newStories: fetchNewPosts()
+		topStories,
+		bestStories,
+		newStories
 	};
 }) satisfies PageLoad;

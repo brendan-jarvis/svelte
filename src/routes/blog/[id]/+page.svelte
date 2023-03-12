96bea1b233240e2eecb5f1c52242fee69dce3d91
Brendan Jarvis <jarvisbrendanj@gmail.com>
2023-03-12 22:31:09 +1300

    Remove second <script>

--- src/routes/blog/[id]/+page.svelte
+++ src/routes/blog/[id]/+page.svelte
@@ -1,16 +1,12 @@
-<script lang='ts'>
-  export const prerender = true;
-  
+<script lang="ts">
+	export const prerender = true;
+
 	export async function load({ params }) {
 		const res = await fetch(`/api/blog/${params.id}`);
 		const post = await res.json();
 
 		return { post };
 	}
-</script>
-
-<script>
-	export let post;
 </script>
 
 <h2>{blogPost.title}</h2>

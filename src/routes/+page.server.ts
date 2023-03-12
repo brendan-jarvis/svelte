f451d20205decbf70027f4579d3bc762a1b18480
Brendan Jarvis <jarvisbrendanj@gmail.com>
2023-03-12 21:41:58 +1300

    Resolve session type error

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,1 +1,31 @@
+import type { PageServerLoad } from './$types';
+import { supabase } from '$lib/supabaseClient';
+
 export const prerender = true;
+
+// TODO: Learn how to handle Supabase session in SvelteKit
+export const load = (async ({ fetch }) => {
+	const fetchBlogPosts = async () => {
+		try {
+			const { data, error } = await supabase
+				.from('posts')
+				.select('*')
+				.limit(10)
+				.order('created_at', { ascending: false })
+				.then(({ data, error }) => ({ data, error }));
+
+			if (error) {
+				throw error;
+			}
+
+			return data;
+		} catch (error) {
+			console.log('Error loading blog posts: ', error);
+		}
+	};
+
+	return {
+		blogPosts: fetchBlogPosts(),
+		session: null
+	};
+}) satisfies PageServerLoad;

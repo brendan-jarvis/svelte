6ede408593468e8f815614d2ad0b5e8f5f7919f7
Brendan Jarvis <jarvisbrendanj@gmail.com>
2023-03-18 14:42:09 +1300

    Update sitemap

--- src/routes/sitemap.xml/+server.ts
+++ src/routes/sitemap.xml/+server.ts
@@ -10,7 +10,24 @@ export async function GET() {
       xmlns:image="https://www.google.com/schemas/sitemap-image/1.1"
       xmlns:video="https://www.google.com/schemas/sitemap-video/1.1"
     >
-      <!-- <url> elements go here -->
+      <url>
+        <loc>https://svelte-brendan-jarvis.vercel.app/</loc>
+        <lastmod>2023-03-18</lastmod>
+        <changefreq>daily</changefreq>
+        <priority>1.0</priority>
+      </url>
+      <url>
+        <loc>https://svelte-brendan-jarvis.vercel.app/blog/1</loc>
+        <lastmod>2023-03-18</lastmod>
+        <changefreq>never</changefreq>
+        <priority>1.0</priority>
+      </url>
+      <url>
+        <loc>https://svelte-brendan-jarvis.vercel.app/hackernews</loc>
+        <lastmod>2023-03-18</lastmod>
+        <changefreq>always</changefreq>
+        <priority>1.0</priority>
+      </url>
     </urlset>`.trim(),
 		{
 			headers: {

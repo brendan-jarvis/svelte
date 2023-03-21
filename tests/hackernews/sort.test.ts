8b6473c1891d319871f13f052335e00b091b1599
Brendan Jarvis <jarvisbrendanj@gmail.com>
2023-03-21 23:51:56 +1300

    Split tests for sorting methods

---
+++ tests/hackernews/sort.test.ts
@@ -0,0 +1,52 @@
+import { expect, test } from '@playwright/test';
+
+test.describe('Hackernews page sort', () => {
+	test.beforeEach(async ({ page }) => {
+		await page.goto('/hackernews');
+	});
+
+	test('by points button sorts posts in descending order', async ({ page }) => {
+		await page.click('button:has-text("Points")');
+
+		const posts = await page.$$('.post');
+
+		const scores = [];
+		for (const post of posts) {
+			const scoreText = await post.innerText('.post-info');
+			const score = parseInt(scoreText.split('•')[1].trim());
+			scores.push(score);
+		}
+
+		expect(scores).toEqual(scores.sort((a, b) => b - a));
+	});
+
+	test('by comments button sorts posts in descending order', async ({ page }) => {
+		await page.click('button:has-text("Comments")');
+
+		const posts = await page.$$('.post');
+
+		const commentCounts = [];
+		for (const post of posts) {
+			const commentText = await post.innerText('.post-info');
+			const commentCount = parseInt(commentText.split('•')[2].trim().split(' ')[0]);
+			commentCounts.push(commentCount);
+		}
+
+		expect(commentCounts).toEqual(commentCounts.sort((a, b) => b - a));
+	});
+
+	test('by date posted button sorts posts in descending order', async ({ page }) => {
+		await page.click('button:has-text("Date posted")');
+
+		const posts = await page.$$('.post');
+
+		const dates = [];
+		for (const post of posts) {
+			const dateText = await post.innerText('.post-info');
+			const date = new Date(dateText.split('•')[3].trim());
+			dates.push(date);
+		}
+
+		expect(dates).toEqual(dates.sort((a, b) => b - a));
+	});
+});
